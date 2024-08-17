084eb87f21139232df0a012b068411969d5bc747
Evan Johnson <evan.johnson.r@gmail.com>
2024-08-16 18:01:31 -0700

    Update player controller and create level_0 scene

--- player/player_movement.gd
+++ player/player_movement.gd
@@ -1,11 +1,23 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var jump_speed = -400
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	var input_direction = Input.get_axis("left", "right")
+	if input_direction:
+		velocity.x = input_direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+		
 	get_input()
 	move_and_slide()
