a6ec1f882783cb7f785d9a5e25028d56a9d3265f
virtual-meme-machine <46010615+virtual-meme-machine@users.noreply.github.com>
2024-08-18 22:31:24 +0000

    Remove Level 3 (forest level)
    - Remove Level 3 (forest level) and update levels 4 and 5 to level 3 and 4 respectively
    - Add pharmacy theme song
    - Pause and resume buttons now play the correct sound effects
    - Trim all sound effect .ogg files to remove empty space
    - Run all scripts through a linter again but this time set it to prefer tabs because Godot's built-in editor uses tabs

--- scenes/components/level_objects/death_plane.gd
+++ scenes/components/level_objects/death_plane.gd
@@ -4,9 +4,9 @@ const DEATH_BOX_SIZE: Vector2 = Vector2(200, 200)
 
 
 func _on_body_entered(body):
-    if body.name == "PlayerNode":
-        TransitionController.reload_current_level(
-            DEATH_BOX_SIZE,
-            body.position,
-            body.rotation_degrees
-        )
+	if body.name == "PlayerNode":
+		TransitionController.reload_current_level(
+			DEATH_BOX_SIZE,
+			body.position,
+			body.rotation_degrees
+		)
--- scenes/components/player/player_controller.gd
+++ scenes/components/player/player_controller.gd
@@ -16,70 +16,70 @@ var gravity                       = ProjectSettings.get_setting("physics/2d/defa
 
 
 func _input(event):
-    if event.is_action_pressed("inhabit"):
-        if scaling_manager.is_inhabited():
-            on_uninhabit()
-        elif not nearbyObjects.is_empty():
-            on_inhabit()
+	if event.is_action_pressed("inhabit"):
+		if scaling_manager.is_inhabited():
+			on_uninhabit()
+		elif not nearbyObjects.is_empty():
+			on_inhabit()
 
 
 func get_input():
-    var input_direction: float = Input.get_axis("left", "right")
+	var input_direction: float = Input.get_axis("left", "right")
 
-    if input_direction > 0:
-        animated_sprite.flip_h = false
-    elif input_direction < 0:
-        animated_sprite.flip_h = true
+	if input_direction > 0:
+		animated_sprite.flip_h = false
+	elif input_direction < 0:
+		animated_sprite.flip_h = true
 
-    if input_direction:
-        velocity.x = input_direction * speed
-    else:
-        velocity.x = move_toward(velocity.x, 0, speed)
+	if input_direction:
+		velocity.x = input_direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 
 func _physics_process(delta):
-    if scaling_manager.is_inhabited():
-        var rb2d: RigidBody2D = scaling_manager.inhabited_node
-        if area2d.overlaps_body(rb2d):
-            velocity.y = jump_speed
-        else:
-            velocity.y = 0
-        velocity.x = 0
-        move_and_slide()
-        return
+	if scaling_manager.is_inhabited():
+		var rb2d: RigidBody2D = scaling_manager.inhabited_node
+		if area2d.overlaps_body(rb2d):
+			velocity.y = jump_speed
+		else:
+			velocity.y = 0
+		velocity.x = 0
+		move_and_slide()
+		return
 
-    if not is_on_floor():
-        velocity.y += gravity * delta
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-    if Input.is_action_just_pressed("jump") and (is_on_floor() || !coyote_timer.is_stopped()):
-        velocity.y = jump_speed
+	if Input.is_action_just_pressed("jump") and (is_on_floor() || !coyote_timer.is_stopped()):
+		velocity.y = jump_speed
 
-    var was_on_floor: bool = is_on_floor()
-    get_input()
-    move_and_slide()
-    if was_on_floor and !is_on_floor():
-        coyote_timer.start()
+	var was_on_floor: bool = is_on_floor()
+	get_input()
+	move_and_slide()
+	if was_on_floor and !is_on_floor():
+		coyote_timer.start()
 
 
 # Area entered signal from area2d
 func _on_area_2d_body_entered(body: Node2D):
-    if body.is_in_group("habitable_objects") and not nearbyObjects.has(body) and body is RigidBody2D:
-        nearbyObjects.append(body as RigidBody2D)
+	if body.is_in_group("habitable_objects") and not nearbyObjects.has(body) and body is RigidBody2D:
+		nearbyObjects.append(body as RigidBody2D)
 
 
 # Area exited signal from area2d
 func _on_area_2d_body_exited(body: Node2D):
-    if body.is_in_group("habitable_objects") and nearbyObjects.has(body):
-        nearbyObjects.erase(body)
+	if body.is_in_group("habitable_objects") and nearbyObjects.has(body):
+		nearbyObjects.erase(body)
 
 
 func on_inhabit():
-    collision2d.set_deferred("disabled", true)
-    hide()
-    inhabit_node.emit(nearbyObjects.back())
+	collision2d.set_deferred("disabled", true)
+	hide()
+	inhabit_node.emit(nearbyObjects.back())
 
 
 func on_uninhabit():
-    collision2d.set_deferred("disabled", false)
-    show()
-    uninhabit_node.emit()
+	collision2d.set_deferred("disabled", false)
+	show()
+	uninhabit_node.emit()
