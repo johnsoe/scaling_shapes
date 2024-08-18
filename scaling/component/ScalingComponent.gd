833ace1bcc4bf5522e3fe082dc1b820df5fd1054
Evan Johnson <evan.johnson.r@gmail.com>
2024-08-17 19:37:29 -0700

    Get inhabiting working better

--- scaling/component/ScalingComponent.gd
+++ scaling/component/ScalingComponent.gd
@@ -3,28 +3,34 @@ extends Node
 @export var scaling_speed = 0.0
 @export var scale_max = 4.0
 @export var scale_min = 0.4
-@export var scale_increment = 0.2
-@export var node_to_scale: Node2D
+@export var scale_increment = 0.02
+
+@export var rb_2d: RigidBody2D
+@export var collision_2d: CollisionShape2D
+@export var sprite_2d: Sprite2D
 
 func _ready():
 	Events.scale_up.connect(on_scale_up)
 	Events.scale_down.connect(on_scale_down)
 
 
-func on_scale_up(node: Node2D):
-	if (node_to_scale == node):
+func on_scale_up(node: RigidBody2D):
+	if (rb_2d == node):
 		scale_helper(scale_increment)
 		
 
-func on_scale_down(node: Node2D):
-	if (node_to_scale == node):
+func on_scale_down(node: RigidBody2D):
+	if (rb_2d == node):
 		scale_helper(scale_increment * -1)
 
 
 func scale_helper(delta):
-	var scale = node_to_scale.scale
+	
 	var updated_scale = Vector2(
-		clamp(scale.x + delta, scale_min, scale_max),
-		clamp(scale.y + delta, scale_min, scale_max)
+		clamp(sprite_2d.scale.x + delta, scale_min, scale_max),
+		clamp(sprite_2d.scale.y + delta, scale_min, scale_max),
 	)
-	node_to_scale.scale = updated_scale
+	rb_2d.mass = updated_scale.x
+	collision_2d.scale = updated_scale
+	sprite_2d.scale = updated_scale
+	
