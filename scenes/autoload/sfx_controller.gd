4108da0b62b3da6a85138b61909a477bea49d206
virtual-meme-machine <46010615+virtual-meme-machine@users.noreply.github.com>
2024-08-19 14:37:43 +0000

    Add more sound effects to gameplay
    - Add sfx_jump.ogg, sfx_jump_land.ogg, and sfx_push_object.ogg sound effect files
    - Add sound effects in game when player jumps, lands from a jump or fall, enters an object, exits an object, or scales an object
    - Route all sound effect audio players to their own channel in the mixer
    - Reset skull and stone_block scaling max to default

--- scenes/autoload/sfx_controller.gd
+++ scenes/autoload/sfx_controller.gd
@@ -3,8 +3,11 @@ extends Node
 @onready var sfx_click_button: AudioStreamPlayer = $SFX_ClickButton
 @onready var sfx_ghost_enter: AudioStreamPlayer = $SFX_GhostEnter
 @onready var sfx_ghost_exit: AudioStreamPlayer = $SFX_GhostExit
+@onready var sfx_jump: AudioStreamPlayer = $SFX_Jump
+@onready var sfx_jump_land: AudioStreamPlayer = $SFX_JumpLand
 @onready var sfx_level_complete: AudioStreamPlayer = $SFX_LevelComplete
 @onready var sfx_pause_game: AudioStreamPlayer = $SFX_PauseGame
+@onready var sfx_push_object: AudioStreamPlayer = $SFX_PushObject
 @onready var sfx_resume_game: AudioStreamPlayer = $SFX_ResumeGame
 @onready var sfx_scale_down: AudioStreamPlayer = $SFX_ScaleDown
 @onready var sfx_scale_up: AudioStreamPlayer = $SFX_ScaleUp
@@ -12,6 +15,10 @@ extends Node
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
+
+
+func is_scale_sfx_playing() -> bool:
+	return sfx_scale_down.playing or sfx_scale_up.playing
 
 
 func play_click_button():
@@ -26,12 +33,24 @@ func play_ghost_exit():
 	sfx_ghost_exit.play()
 
 
+func play_jump():
+	sfx_jump.play()
+
+
+func play_jump_land():
+	sfx_jump_land.play()
+
+
 func play_level_complete():
 	sfx_level_complete.play()
 
 
 func play_pause_game():
 	sfx_pause_game.play()
+
+
+func play_push_object():
+	sfx_push_object.play()
 
 
 func play_resume_game():
@@ -44,3 +63,7 @@ func play_scale_down():
 
 func play_scale_up():
 	sfx_scale_up.play()
+
+
+func stop_push_object():
+	sfx_push_object.stop()
