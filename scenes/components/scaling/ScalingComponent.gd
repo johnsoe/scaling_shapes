f07f1f1ef0cdc440492282f34f494e2e421ba949
Evan Johnson <evan.johnson.r@gmail.com>
2024-08-19 07:49:07 -0700

    Shovel, skull, box, crate, and boulder with faces
    Co-authored-by: virtual-meme-machine <46010615+virtual-meme-machine@users.noreply.github.com>

--- scenes/components/scaling/ScalingComponent.gd
+++ scenes/components/scaling/ScalingComponent.gd
@@ -1,40 +1,70 @@
 extends Node
 
-@export var scaling_speed: float = 0.0
 @export var scale_max: float = 4.0
 @export var scale_min: float = 0.4
 @export var scale_increment: float = 0.02
+
 @export var rb_2d: RigidBody2D
 @export var collision_2d: CollisionShape2D
-@export var sprite_2d: Sprite2D
+@export var animated_sprite: AnimatedSprite2D
 
+var is_inhabited = false
+var current_scaling = 0.0
 
 func _ready():
 	Events.scale_up.connect(on_scale_up)
 	Events.scale_down.connect(on_scale_down)
+	Events.scale_pause.connect(on_scale_pause)
+	Events.node_inhabited.connect(on_node_inhabited)
+	Events.node_uninhabited.connect(on_node_uninhabited)
+	if animated_sprite != null:
+		animated_sprite.play("default")
 
+
+func _physics_process(_delta):
+	if not is_inhabited:
+		return
+		
+	var updated_scale: Vector2 = Vector2(
+		clamp(animated_sprite.scale.x + current_scaling, scale_min, scale_max),
+		clamp(animated_sprite.scale.y + current_scaling, scale_min, scale_max),
+	)
+  
+  if animated_sprite.scale > updated_scale and not SFXController.is_scale_sfx_playing():
+		SFXController.play_scale_down()
+	elif animated_sprite.scale < updated_scale and not SFXController.is_scale_sfx_playing():
+		SFXController.play_scale_up()
+  
+	rb_2d.mass = updated_scale.x
+	collision_2d.scale = updated_scale
+	animated_sprite.scale = updated_scale
+	
 
 func on_scale_up(node: RigidBody2D):
 	if (rb_2d == node):
-		scale_helper(scale_increment)
+		animated_sprite.play("inflate")
+		current_scaling = scale_increment
 
 
 func on_scale_down(node: RigidBody2D):
 	if (rb_2d == node):
-		scale_helper(scale_increment * -1)
+		animated_sprite.play("deflate")
+		current_scaling = scale_increment * -1
 
 
-func scale_helper(delta):
-	var updated_scale: Vector2 = Vector2(
-									 clamp(sprite_2d.scale.x + delta, scale_min, scale_max),
-									 clamp(sprite_2d.scale.y + delta, scale_min, scale_max),
-								 )
+func on_scale_pause(node: RigidBody2D):
+	if (rb_2d == node):
+		animated_sprite.play("neutral")
+		current_scaling = 0.0
 
-	if sprite_2d.scale > updated_scale and not SFXController.is_scale_sfx_playing():
-		SFXController.play_scale_down()
-	elif sprite_2d.scale < updated_scale and not SFXController.is_scale_sfx_playing():
-		SFXController.play_scale_up()
 
-	rb_2d.mass = updated_scale.x
-	collision_2d.scale = updated_scale
-	sprite_2d.scale = updated_scale
+func on_node_inhabited(node: RigidBody2D):
+	if (rb_2d == node):
+		animated_sprite.play("neutral")
+		is_inhabited = true
+
+
+func on_node_uninhabited(node: RigidBody2D):
+	if (rb_2d == node):
+		animated_sprite.play("default")
+		is_inhabited = false
--- scenes/components/scaling/ScalingManager.gd
+++ scenes/components/scaling/ScalingManager.gd
@@ -8,16 +8,21 @@ func is_inhabited() -> bool:
 	return inhabited_node != null
 
 
-func _process(_delta):
+func _input(_delta):
 	if Input.is_action_pressed("up") and is_inhabited():
 		Events.emit_scale_up(inhabited_node)
 	elif Input.is_action_pressed("down") and is_inhabited():
 		Events.emit_scale_down(inhabited_node)
+	elif Input.is_action_just_released("down") or Input.is_action_just_released("up"):
+		Events.emit_scale_pause(inhabited_node)
 
 
 func _on_player_node_inhabit_node(node):
+	Events.emit_node_inhabited(node)
 	inhabited_node = node
 
 
 func _on_player_node_uninhabit_node():
+	Events.emit_scale_pause(inhabited_node)
+	Events.emit_node_uninhabited(inhabited_node)
 	inhabited_node = null
