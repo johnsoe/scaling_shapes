cd2010df2b2ae522c23d5f8e031678f2eba6f83d
Evan Johnson <evan.johnson.r@gmail.com>
2024-08-19 00:41:06 -0700

    Shovel, skull, box, crate, and boulder with faces

--- scenes/autoload/events.gd
+++ scenes/autoload/events.gd
@@ -2,6 +2,9 @@ extends Node
 
 signal scale_up(node: RigidBody2D)
 signal scale_down(node: RigidBody2D)
+signal scale_pause(node: RigidBody2D)
+signal node_inhabited(node: RigidBody2D)
+signal node_uninhabited(node: RigidBody2D)
 
 
 func emit_scale_up(node: RigidBody2D):
@@ -10,3 +13,15 @@ func emit_scale_up(node: RigidBody2D):
 
 func emit_scale_down(node: RigidBody2D):
 	scale_down.emit(node)
+
+
+func emit_scale_pause(node: RigidBody2D):
+	scale_pause.emit(node)
+	
+
+func emit_node_inhabited(node: RigidBody2D):
+	node_inhabited.emit(node)
+	
+
+func emit_node_uninhabited(node: RigidBody2D):
+	node_uninhabited.emit(node)
--- scenes/components/scaling/ScalingComponent.gd
+++ scenes/components/scaling/ScalingComponent.gd
@@ -1,36 +1,64 @@
 extends Node
 
-@export var scaling_speed: float = 0.0
 @export var scale_max: float = 4.0
 @export var scale_min: float = 0.4
 @export var scale_increment: float = 0.02
+
 @export var rb_2d: RigidBody2D
 @export var collision_2d: CollisionShape2D
-@export var sprite_2d: Sprite2D
+@export var animated_sprite: AnimatedSprite2D
 
+var is_inhabited = false
+var current_scaling = 0.0
 
 func _ready():
 	Events.scale_up.connect(on_scale_up)
 	Events.scale_down.connect(on_scale_down)
+	Events.scale_pause.connect(on_scale_pause)
+	Events.node_inhabited.connect(on_node_inhabited)
+	Events.node_uninhabited.connect(on_node_uninhabited)
+	if animated_sprite != null:
+		animated_sprite.play("default")
 
+
+func _physics_process(_delta):
+	if not is_inhabited:
+		return
+		
+	var updated_scale: Vector2 = Vector2(
+		clamp(animated_sprite.scale.x + current_scaling, scale_min, scale_max),
+		clamp(animated_sprite.scale.y + current_scaling, scale_min, scale_max),
+	)
+	rb_2d.mass = updated_scale.x
+	collision_2d.scale = updated_scale
+	animated_sprite.scale = updated_scale
+	
 
 func on_scale_up(node: RigidBody2D):
 	if (rb_2d == node):
-		scale_helper(scale_increment)
+		animated_sprite.play("inflate")
+		current_scaling = scale_increment
 
 
 func on_scale_down(node: RigidBody2D):
 	if (rb_2d == node):
-		scale_helper(scale_increment * -1)
+		animated_sprite.play("deflate")
+		current_scaling = scale_increment * -1
 
 
-func scale_helper(delta):
+func on_scale_pause(node: RigidBody2D):
+	if (rb_2d == node):
+		animated_sprite.play("neutral")
+		current_scaling = 0.0
 
-	var updated_scale: Vector2 = Vector2(
-									 clamp(sprite_2d.scale.x + delta, scale_min, scale_max),
-									 clamp(sprite_2d.scale.y + delta, scale_min, scale_max),
-								 )
-	rb_2d.mass = updated_scale.x
-	print(rb_2d.mass)
-	collision_2d.scale = updated_scale
-	sprite_2d.scale = updated_scale
+
+func on_node_inhabited(node: RigidBody2D):
+	if (rb_2d == node):
+		animated_sprite.play("neutral")
+		is_inhabited = true
+
+
+func on_node_uninhabited(node: RigidBody2D):
+	if (rb_2d == node):
+		animated_sprite.play("default")
+		is_inhabited = false
